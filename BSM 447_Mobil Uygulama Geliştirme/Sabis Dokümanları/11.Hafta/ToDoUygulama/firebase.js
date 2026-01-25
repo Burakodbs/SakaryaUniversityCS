31ce012a3f627037f4ead907e974bb7006bc3db7
Burak Odabaş <burak.odabas2@ogr.sakarya.edu.tr>
2026-01-25 06:51:08 +0300

    Remove Firebase credentials from config files
    Cleared sensitive Firebase configuration values from firebase.js files in both ToDoUygulama and w12-Firebase-Login-SignUp directories to enhance security and prevent accidental exposure of credentials.

--- BSM 447_Mobil Uygulama Geliştirme/Sabis Dokümanları/11.Hafta/ToDoUygulama/firebase.js
+++ BSM 447_Mobil Uygulama Geliştirme/Sabis Dokümanları/11.Hafta/ToDoUygulama/firebase.js
@@ -3,21 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-
-  apiKey: "AIzaSyCzh4ssvSoNGdBUt1InneX44WWA41gw0ys",
-
-  authDomain: "mug23-19001.firebaseapp.com",
-
-  projectId: "mug23-19001",
-
-  storageBucket: "mug23-19001.appspot.com",
-
-  messagingSenderId: "75090662092",
-
-  appId: "1:75090662092:web:844c5787131c08e185e869",
-
-  measurementId: "G-361VPK6C5L"
-
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
--- BSM 447_Mobil Uygulama Geliştirme/Yeni klasör/w12-Firebase-Login-SignUp/firebase.js
+++ BSM 447_Mobil Uygulama Geliştirme/Yeni klasör/w12-Firebase-Login-SignUp/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDOtXGsHRbk9uUUoAD-iLLrmmRfRLSQ4Vs",
-  authDomain: "falt-f2547.firebaseapp.com",
-  projectId: "falt-f2547",
-  storageBucket: "falt-f2547.firebasestorage.app",
-  messagingSenderId: "978801041748",
-  appId: "1:978801041748:web:527d4826aa93c8ac7428c1",
-  measurementId: "G-DDFXTM2505"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 if (!firebase.apps.length) {
